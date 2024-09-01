303b0ae41d21e6baec9427b21c0c25daba94dd79
shikarinkato <ramanjadaun@gmail.com>
2024-09-01 16:26:39 +0530

    Production Server Choosen

--- src/context/StateProvider.js
+++ src/context/StateProvider.js
@@ -5,8 +5,8 @@ import UserIcon from "../assests/img/userIcon.png";
 
 export const Context = createContext();
 let serverUrl;
-// serverUrl = "https://social-media-backend-tau-six.vercel.app/";
-serverUrl = "http://localhost:5000/api/v1";
+serverUrl = "https://social-media-backend-tau-six.vercel.app/";
+// serverUrl = "http://localhost:5000/api/v1";
 
 export function StateProvider({ children }) {
   const navigate = useNavigate();
